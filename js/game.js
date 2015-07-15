66032dfdd32332aae77fc6ad03e3ac4e25634eeb
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2015-07-15 13:13:49 +0000

    randomize items!

---
+++ js/game.js
@@ -0,0 +1,16 @@
+$.fn.randomize = function(selector){
+    var $elems = selector ? $(this).find(selector) : $(this).children(),
+        $parents = $elems.parent();
+
+    $parents.each(function(){
+        $(this).children(selector).sort(function(){
+            return Math.round(Math.random()) - 0.5;
+        }).detach().appendTo(this);
+    });
+
+    return this;
+};
+
+$(document).ready(function(){
+  $("ul").randomize('li');
+});
